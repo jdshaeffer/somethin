d52b204c2abe0f3c3a5da32f5f158a648a60be4b
jdshaeffer <shaefferjd@gmail.com>
2019-11-07 22:27:26 -0700

    item class

---
+++ item.js
@@ -0,0 +1,9 @@
+class Item {
+    constructor(name, info, indef) {
+        this.name = name
+        this.info = info
+        this.indef = indef // indefinite article of item
+    }
+}
+
+let apple = new Item("apple", "it's an apple", "an")
